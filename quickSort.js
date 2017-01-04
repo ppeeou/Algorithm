9cb2ed34360c96b5fce5f12abee3a4eed6c3043f
JoHyunWoo <xodltus@naver.com>
2017-01-04 23:26:43 +0900

    quick - 설명 추가

--- quickSort.js
+++ quickSort.js
@@ -41,5 +41,6 @@ var quickSort = function (array, left, right) {
     return array;
 };
 
-
+//퀵정렬은 같은 숫자를 정렬할 때 순서가 바뀔수 있음 2와 2를 정렬할때 바뀔수도 있음...
+//최악일 경우는 가장 큰수 or 가장 작은 수 가 걸렸을 때...
 console.log(quickSort([54, 6, 645, 67, 3, 5, 7, 856, 8983, 9, 657, 57, 1, 567, 2]));
