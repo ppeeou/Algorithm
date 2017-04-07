5ae5e3a0a0399c60d9225222fcfebe76579d736c
JoHyunWoo <xodltus@naver.com>
2017-04-07 17:05:31 +0900

    curry prototype

---
+++ dataStructure/curry.js
@@ -0,0 +1,25 @@
+
+
+Function.prototype.method = function (name,value){ 
+    return this.prototype[name] = value; 
+}
+
+Function.method('curry', function () {
+
+    var slice = Array.prototype.slice,
+        args = slice.apply(arguments), 
+        that = this;
+
+    return function () {
+        return that.apply(null, args.concat(slice.apply(arguments)));
+    }
+})
+
+var add = function (a, b) {
+    return a + b;
+}
+
+
+var add1 = add.curry(1);
+
+console.log(add1(6));
